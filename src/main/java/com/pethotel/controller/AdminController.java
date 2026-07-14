409fca0883cd39be81cb092d55c9c2fdfeb512d4
NamJB <njb3430@naver.com>
2026-07-14 17:21:16 +0900

    관리자 페이지

---
+++ src/main/java/com/pethotel/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.pethotel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+
+	@GetMapping("/page")
+	public String admin() {
+		
+		return "admin/page";
+		
+	}
+}
